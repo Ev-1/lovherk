8aecc5d482b63c8fa5f726021da7253370c83d56
Ev-1 <33662061+Ev-1@users.noreply.github.com>
2018-08-09 16:58:56 +0200

    still no 3.6 lel

--- utils/default.py
+++ utils/default.py
@@ -12,27 +12,3 @@ def get(file):
         raise AttributeError("Unknown argument")
     except FileNotFoundError:
         raise FileNotFoundError("JSON file wasn't found")
-
-
-def timetext(name):
-    return f"{name}_{int(time.time())}.txt"
-
-
-def date(target):
-    return target.strftime("%d %B %Y, %H:%M")
-
-
-def responsible(target, reason):
-    responsible = f"[ {target} ]"
-    if reason is None:
-        return f"{responsible} no reason given..."
-    return f"{responsible} {reason}"
-
-
-def actionmessage(case, mass=False):
-    output = f"**{case}** the user"
-
-    if mass is True:
-        output = f"**{case}** the IDs/Users"
-
-    return f"✅ Successfully {output}"
